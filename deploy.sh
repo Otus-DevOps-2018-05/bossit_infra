9af389e4b16c0583933a5b3ad1823d2ca660cab3
Dinar Mukhametshin <dinar.mukhametshin@gmail.com>
2018-06-26 17:28:02 +0300

    Rename startup script and update readme.md

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# get src from repo
 cd /home/bossit/
 git clone -b monolith https://github.com/express42/reddit
 
-## make build and start puma server
 cd reddit
 bundle install
 puma -d
