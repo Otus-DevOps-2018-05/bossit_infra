d5a0fcc86075ac12257d40f4b89af214e607194b
Dinar Mukhametshin <dinar.mukhametshin@gmail.com>
2018-07-16 17:48:42 +0300

    new input variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,12 @@ variable public_key_path {
 variable disk_image {
   description = "Disk image"
 }
+
+variable private_key_path {
+  description = "Path to the private key used for ssh access"
+}
+
+variable "zone" {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
