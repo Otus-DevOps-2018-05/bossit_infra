9cdfc207c98488d2359c11d6d86b83c2188ec955
Dinar Mukhametshin <dinar.mukhametshin@gmail.com>
2018-07-24 16:34:01 +0300

    add ssh config in `vpc` module

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,12 +1,11 @@
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
+resource "google_compute_firewall" "firewall_ssh" {
+  name    = "default-allow-ssh"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["9292"]
+    ports    = ["22"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
+  source_ranges = "${var.source_ranges}"
 }
