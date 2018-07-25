3644623bf1a2637f7e24199272bea13cdc3c8126
Dinar Mukhametshin <dinar.mukhametshin@gmail.com>
2018-07-25 16:34:38 +0300

    add output variables

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -2,7 +2,6 @@ output "app_external_ip" {
   value = "${module.app.app_external_ip}"
 }
 
-# output "app_load_balancing" {
-#   value = "${google_compute_forwarding_rule.reddit-lb.ip_address}"
-# }
-
+output "db_external_ip" {
+  value = "${module.db.db_external_ip}"
+}
